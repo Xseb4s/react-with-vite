dd7e7a8f3c1131c77352d197038d4c5130283397
Xseb4s <sebas25qr@gmail.com>
2023-11-07 14:16:58 -0500

    created the card, layout and navbar component, added API to get data and blank pages

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react";
 import Layout from "../../components/layout/layout.components";
 import Card from "../../components/card/card.components";
+import { apiUrl } from '../../api/getProducts';
+
 const Home = () => {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`${apiUrl}/products`)
+            const data = await response.json()
+            setItems(data)
+          } catch (error) {
+            console.error(`Error en la fetchData: ${error}`);
+          }
+        }
+        fetchData()
+      }, [])
     return(
         <Layout>
             <h1>Home</h1>
-            <Card/>
+            {items?.map(item => (
+            <Card
+              key={item.id}
+              data={item}
+            />
+          ))
+        }
         </Layout>
     )
 }
