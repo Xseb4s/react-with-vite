37f0134d7e3c7c4238481e1e7c32aa5cf08b4827
Xseb4s <sebas25qr@gmail.com>
2024-01-05 14:57:20 -0500

    feat: added the sign in interface and the users log function

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,8 +1,76 @@
+import { useContext, useState } from "react";
 import Layout from "../../components/layout/layout.components";
+import { ShoppingCartContext } from "../../context";
+import { EnvelopeIcon, EyeIcon, EyeSlashIcon, KeyIcon } from "@heroicons/react/24/outline";
 const SignIn = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const {account} = useContext(ShoppingCartContext);
+
+    const localAccount = localStorage.getItem("account");
+    const parsedAccount = JSON.parse(localAccount);
+
+    const noAccountStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+    const noAccountState = account ? Object.keys(account).length === 0 : true;
+    const hasUserAccount = !noAccountStorage || !noAccountState;
+    console.log(hasUserAccount, parsedAccount)
+
     return(
         <Layout>
-            <h1>Sign In</h1>
+            <h1 className="mb-10">Sign In</h1>
+            <div className="flex flex-col gap-4 bg-gray-300 p-3 rounded-lg w-96">
+                <p className="flex justify-between gap-1 w-full">
+                    <span 
+                        className="flex items-center border-2 pr-1 rounded-l-lg font-bold text-sm bg-white" 
+                        style={{color:"#2f4f4f", borderColor:"#2f4f4f"}}>
+                            <EnvelopeIcon className="w-8 px-1 font-medium"/>
+                        Email
+                    </span>
+                    <span 
+                        className="ml-7 pl-4 w-full rounded-r-lg font-bold text-md border" 
+                        style={{color:"#2f4f4f", borderColor:"#2f4f4f"}}>
+                            {parsedAccount?.email}
+                    </span> 
+                </p>
+                <p className="flex justify-between gap-1 w-full">
+                    <span 
+                        className="flex items-center border-2 pr-1 rounded-l-lg font-bold text-sm bg-white" 
+                        style={{color:"#2f4f4f", borderColor:"#2f4f4f"}}>
+                            <KeyIcon className="w-8 px-1 font-medium"/>
+                        Password
+                    </span>
+                    <div className="flex justify-between w-50 border rounded-r-lg" style={{color:"#2f4f4f", borderColor:"#2f4f4f"}}>
+                        <input 
+                            style={{}}
+                            className="flex w-[80%] pl-4 font-bold text-lg  focus:outline-none bg-transparent"
+                            readOnly
+                            type={showPassword ? "text" : "password"}
+                            value={parsedAccount?.password}
+                        />
+                        <button
+                            style={{backgroundColor:"#2f4f4f"}}
+                            className="px-2 rounded-r-lg border"
+                            aria-label="toggle password visibility"
+                            onClick={() => setShowPassword(!showPassword)}>
+                            {showPassword ? <EyeIcon className="w-5 font-bold text-white"/> : <EyeSlashIcon className="w-5 font-bold text-white"/>}
+                        </button>
+                    </div>
+                </p>
+                <div className="flex justify-between gap-2 mt-10">
+                    <button
+                        className="border p-1 rounded-lg font-semibold w-1/2"
+                        style={{borderColor:"#2f4f4f",color:"#2f4f4f"}}
+                        disabled={hasUserAccount}>
+                        Sign Up
+                    </button>
+                    <button 
+                        className={`border p-1 rounded-lg font-bold text-white w-1/2 ${!hasUserAccount ? "opacity-30" : null }`}
+                        style={{borderColor:"#2f4f4f", backgroundColor:"#2f4f4f"}}
+                        disabled={!hasUserAccount}>
+                        Login
+                    </button>
+                </div>
+            </div>
         </Layout>
     )
 }
--- src/components/navbar/navbar.components.jsx
+++ src/components/navbar/navbar.components.jsx
@@ -7,7 +7,7 @@ import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 
 const Navbar = () => {
-    const {count, setSearchByCategory, setSignOut} = useContext(ShoppingCartContext);
+    const {count, setSearchByCategory, signOut, setSignOut} = useContext(ShoppingCartContext);
     const handleNavLinkClick = (item) => {
         if (item.href !== "/") {
             setSearchByCategory(item.href);
@@ -15,28 +15,14 @@ const Navbar = () => {
             setSearchByCategory(null);
         }
     };
-    const handleSignOut = () =>{
-        const stringifiedSignOut = JSON.stringify(true)
-        localStorage.setItem("sign-out", stringifiedSignOut)
-        setSignOut(true)
-    }
-    const activeStyle = 'underline underline-offset-4';
+    const isSingOut = localStorage.getItem("sign-out");
+    const parsedSignOut = JSON.parse(isSingOut)
+    const isUserSignOut = signOut || parsedSignOut
 
-    return(
-        <nav className="flex justify-between fixed top-0 z-99 w-full p-4">
-            <ul className=" flex flex-row gap-4">
-                {menu.map((item, i) => (
-                    <li key={i} className={`text-white ${item.title.toLocaleLowerCase() === 'shopi' ? 'font-bold':''}`}>
-                        <NavLink 
-                            onClick={() => handleNavLinkClick(item)}
-                            to={item.href}
-                            className={({isActive}) =>isActive ? activeStyle : undefined}>
-                            {item.title}
-                        </NavLink>
-                    </li>
-                ))}
-            </ul>
-            <ul className=" flex flex-row gap-4">
+    const rederView = () => {
+        if(isUserSignOut){
+            return(
+                <>
                 <li className="text-white/60">
                     srodriguez44482@gmail.com
                 </li>
@@ -55,9 +41,46 @@ const Navbar = () => {
                                 {item.title}
                             </NavLink>
                         )
-                        }                        
+                    }                        
                     </li>
                 ))}
+                </>
+            )
+        } else{
+            return(
+                <li className="text-white">
+                    <NavLink to="/sign-in" className={({isActive}) =>isActive ? activeStyle : undefined}>
+                        Sign In
+                    </NavLink>
+                </li>
+            )
+        }
+    }
+
+    const handleSignOut = () =>{
+        const stringifiedSignOut = JSON.stringify(false)
+        localStorage.setItem("sign-out", stringifiedSignOut)
+        setSignOut(false)
+    }
+    const activeStyle = 'underline underline-offset-4';
+
+    return(
+        <nav className="flex justify-between fixed top-0 z-99 w-full p-4">
+            <ul className=" flex flex-row gap-4">
+                {menu.map((item, i) => (
+                    <li key={i} className={`text-white ${item.title.toLocaleLowerCase() === 'shopi' ? 'font-bold':''}`}>
+                        <NavLink 
+                            onClick={() => handleNavLinkClick(item)}
+                            to={item.href}
+                            className={({isActive}) =>isActive ? activeStyle : undefined}>
+                            {item.title}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+            <ul className=" flex flex-row gap-4">
+                
+                {rederView()}
                 <li>
                     <ShoppingCartIcon className="w-6 h-6 text-gray-300"/> <strong className="flex justify-center items-center bg-white absolute top-2 right-2 border-0 rounded-full w-4 h-4">{count}</strong>    
                 </li>
